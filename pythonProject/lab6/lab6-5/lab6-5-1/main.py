0fa28b6df9626f7a11e6c79b259ac5c08c13142c
Александр <tepteev2017@mail.ru>
2023-12-10 16:26:26 +0500

    Add lab8 sort

--- pythonProject/lab6/lab6-5/lab6-5-1/main.py
+++ pythonProject/lab6/lab6-5/lab6-5-1/main.py
@@ -5,22 +5,29 @@ printRectangle(-1, 5, "output")
 PrintSquare(3, "output1")
 
 # Test 1:
-module_func.printRectangle(4, 0, "output")
-module_func.PrintSquare(3, "output1")
+printRectangle(4, 0, "output")
+PrintSquare(3, "output1")
 print("test 2")
+
+
 # Test 2:
-module_func.printRectangle(0, 0, "output")
-module_func.PrintSquare(0, "output1")
+printRectangle(0, 0, "output")
+PrintSquare(0, "output1")
 print("test 3")
+
+
 # Test 3:
-module_func.printRectangle(-100, 0, "output")
-module_func.PrintSquare(-3, "output1")
+printRectangle(-100, 0, "output")
+PrintSquare(-3, "output1")
 print("test 4")
+
 # Test 4:
-module_func.printRectangle(4, 5, "output")
-module_func.PrintSquare(3, "output1")
+printRectangle(4, 5, "output")
+PrintSquare(3, "output1")
 print("Complete")
 print("test 5")
+
+
 # Test 5:
-module_func.printRectangle(0, 0, "output")
-module_func.PrintSquare(0, "output1")
+printRectangle(0, 0, "output")
+PrintSquare(0, "output1")
