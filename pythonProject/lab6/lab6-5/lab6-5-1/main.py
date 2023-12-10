c9684b96ea25df427b20e1e001c77eb1e30f907b
Александр <tepteev2017@mail.ru>
2023-12-10 19:01:05 +0500

    ended lab6

--- pythonProject/lab6/lab6-5/lab6-5-1/main.py
+++ pythonProject/lab6/lab6-5/lab6-5-1/main.py
@@ -1,33 +1,33 @@
 from module_func import *
 
 # основная программа
-printRectangle(-1, 5, "output")
+printRectangle(1, 5, "output")
 PrintSquare(3, "output1")
 
-# Test 1:
-printRectangle(4, 0, "output")
-PrintSquare(3, "output1")
-print("test 2")
-
-
-# Test 2:
-printRectangle(0, 0, "output")
-PrintSquare(0, "output1")
-print("test 3")
-
-
-# Test 3:
-printRectangle(-100, 0, "output")
-PrintSquare(-3, "output1")
-print("test 4")
-
-# Test 4:
-printRectangle(4, 5, "output")
-PrintSquare(3, "output1")
-print("Complete")
-print("test 5")
-
-
-# Test 5:
-printRectangle(0, 0, "output")
-PrintSquare(0, "output1")
+# # Test 1:
+# printRectangle(4, 0, "output")
+# PrintSquare(3, "output1")
+# print("test 2")
+#
+#
+# # Test 2:
+# printRectangle(0, 0, "output")
+# PrintSquare(0, "output1")
+# print("test 3")
+#
+#
+# # Test 3:
+# printRectangle(-100, 0, "output")
+# PrintSquare(-3, "output1")
+# print("test 4")
+#
+# # Test 4:
+# printRectangle(4, 5, "output")
+# PrintSquare(3, "output1")
+# print("Complete")
+# print("test 5")
+#
+#
+# # Test 5:
+# printRectangle(0, 0, "output")
+# PrintSquare(0, "output1")
